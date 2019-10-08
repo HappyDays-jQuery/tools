c11c2f337495cfef0040d3c3ee15957c52c359c7
HappyDays-jQuery <developer@jquery.tokyo>
2019-10-08 17:53:15 +0900

    各ページのベース作成

--- tools/src/App.vue
+++ tools/src/App.vue
@@ -1,9 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-card class="fill-height">
+      <v-navigation-drawer expand-on-hover absolute>
+        <v-list nav dense>
+          <v-list-item link to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <v-list-item link to="/about">
+            <v-list-item-icon>
+              <v-icon>mdi-panda</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+          <v-list-item link to="/decode">
+            <v-list-item-icon>
+              <v-icon>mdi-dog</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Decoder</v-list-item-title>
+          </v-list-item>
+          <v-list-item link to="/encode">
+            <v-list-item-icon>
+              <v-icon>mdi-cat</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Encode</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-content>
+        <v-container>
+          <router-view/>
+        </v-container>
+      </v-content>
+    </v-card>
+
+  </v-app>
 </template>
+
+<script>
+  export default {
+    name: 'AppComponent',
+
+    props: {},
+
+    data: () => ({
+      drawer: false,
+    }),
+
+    mounted() {
+    },
+
+    methods: {}
+  }
+</script>
--- tools/src/router.js
+++ tools/src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Encode from './views/Encode.vue'
+import Decode from './views/Decode.vue'
+
 
 Vue.use(Router)
 
@@ -8,18 +12,9 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    {path: '/', name: 'home', component: Home},
+    {path: '/about', name: 'about', component: About},
+    {path: '/encode', name: 'encode', component: Encode},
+    {path: '/decode', name: 'decode', component: Decode},
   ]
 })
