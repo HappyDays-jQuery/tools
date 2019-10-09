d6884f8f4a96b4d4263d57ad4c357113099807d2
HappyDays-jQuery <developer@jquery.tokyo>
2019-10-09 15:36:30 +0900

    base６４のみ実装

--- tools/src/views/Decode.vue
+++ tools/src/views/Decode.vue
@@ -1,5 +1,103 @@
 <template>
-  <div class="about">
-    <h1>This is an Decode page</h1>
+  <div class="decode">
+    <h1>Decode</h1>
+    <v-textarea
+      v-model="input"
+      auto-grow
+      clearable
+      counter
+      filled
+      label="Input text"
+      persistent-hint="persistentHint"
+      placeholder="デコードしたい文字列を入力してください。"
+      rounded
+      row-height="40"
+      rows="4"
+      shaped
+    ></v-textarea>
+
+    <v-btn class="ma-2" x-large color="indigo" v-on:click="decodeBase64" outlined>Decode Base64</v-btn>
+    <v-btn class="ma-2" x-large color="teal" outlined>Decode URL</v-btn>
+    <v-btn class="ma-2" x-large color="success" outlined>unicode unescape</v-btn>
+
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title>サンプル</v-list-item-title>
+        <v-list-item-subtitle>
+          <b>[base64]</b> YWJjZGVmZw==
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          <b>[URL Encode]</b> %E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          <b>[unicode]</b> \u30b5\u30f3\u30d7\u30eb
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-textarea
+      v-model="result"
+      auto-grow
+      clearable
+      counter
+      filled
+      label="Decoded Result"
+      rounded
+      row-height="40"
+      rows="4"
+      shaped
+    ></v-textarea>
+
+    <info-text/>
   </div>
 </template>
+
+<script>
+  export default {
+
+    name: 'Decode',
+
+    components: {},
+
+    data: () => ({
+      input: '',
+      result: '',
+    }),
+
+    props: [],
+
+    created() {
+      this.debug('Decode Component created.')
+      this.initialize()
+    },
+
+    mounted() {
+      this.debug('Decode Component mounted.')
+    },
+
+    methods: {
+      initialize: function () {
+        this.input = ''
+        this.result = ''
+      },
+      checkBase64: function (str) {
+        return str.match(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/)
+      },
+      decodeBase64: function () {
+        if(this.input === '') {
+          this.result = "入力してください。"
+          return
+        }
+
+        let val = this.input
+        let valss = val.replace(/-/g, '+').replace(/_/g, '/').replace(/\./g, '')
+
+        if (!this.checkBase64(valss)) {
+          this.result = "変換できませんでした。入力されたデータがBase64でない可能性があります。"
+          return
+        }
+        this.result = decodeURIComponent(escape(atob(valss)))
+      }
+    },
+
+  }
+</script>
