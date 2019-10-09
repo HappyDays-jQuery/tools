30f2710090befde325adbb4e5ef424537c89435a
HappyDays-jQuery <developer@jquery.tokyo>
2019-10-09 17:46:07 +0900

    DEBUG変数追加

---
+++ tools/src/js/common.vue
@@ -0,0 +1,12 @@
+<script>
+  export default {
+    methods: {
+      debug(msg) {
+        if (process.env.MIX_DEBUG) {
+          // eslint-disable-next-line no-console
+          console.log(msg)
+        }
+      }
+    }
+  }
+</script>
